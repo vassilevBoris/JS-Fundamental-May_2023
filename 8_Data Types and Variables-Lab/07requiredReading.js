2fd5410b48db1136de9a3487f1beb6c972907df2
Boris Vassilev <vassilev.boris@gmail.com>
2023-05-19 16:21:30 +0400

    Add solved tasks

--- 8_Data Types and Variables-Lab/07requiredReading.js
+++ 8_Data Types and Variables-Lab/07requiredReading.js
@@ -1,5 +1,9 @@
 function requiredReading(numOfPages, pagesPerHour, daysPerBook) {
     
+    let totalTimePerBookInHour = numOfPages / pagesPerHour;
+    let hoursPerDay = totalTimePerBookInHour / daysPerBook;
+
+    console.log(hoursPerDay);
 }
-requiredReading(212, 20 , 2);
-// requiredReading(432, 15 , 4);
+// requiredReading(212, 20 , 2);
+requiredReading(432, 15 , 4);
--- 8_Data Types and Variables-Lab/08centuriesToMinutes.js
+++ 8_Data Types and Variables-Lab/08centuriesToMinutes.js
@@ -1,5 +1,10 @@
 function centuriesToMinutes(centuries) {
-    
+    const yearInDays = 365.2422;
+    let years = centuries * 100;
+    let days = Math.trunc(years * yearInDays);
+    let hours = days * 24;
+    let minutes = hours * 60;
+    console.log(`${centuries} centuries = ${years} years = ${days} days = ${hours} hours = ${minutes} minutes`);
 }
-centuriesToMinutes(1);
-// centuriesToMinutes(5);
+// centuriesToMinutes(1);
+centuriesToMinutes(5);
